c95b46d366980de2ddfd599a692e4ee673ee1878
MFSKS103 <maoriciofarrel.s@gmail.com>
2024-11-27 13:06:41 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+st.subheader("Hi, I am Momo :wave:")
+st.title("Percobaan")
+st.write("Masih coba-coba buat website")
+st.write("[Learn More >](https://youtu.be/dQw4w9WgXcQ?si=mg2XGSCUNmCN8l2o)")
